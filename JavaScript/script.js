0ff05b88cba4c18c5e3a4a4f12ac59bf5e152cfa
Fawzaan <fawzi20112001@gmail.com>
2022-02-23 09:51:09 +0530

    Completed second assignment

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -24,3 +24,10 @@ function showResult() {
 
     document.getElementById("result").innerHTML = "The answer is " + result;
 }
+
+function showFullName() {
+    let firstName = document.getElementById("first-name").value;
+    let secondName = document.getElementById("second-name").value;
+
+    document.getElementById("full-name-result").innerHTML = "Your Full Name is " + firstName + " " + secondName;
+}
