568f0e2c37a2aa8c49716fe66c38e27372c9d43d
Fawzaan <fawzi20112001@gmail.com>
2022-03-16 10:50:26 +0530

    Third assignment complete

--- DOM Handling/script.js
+++ DOM Handling/script.js
@@ -1,3 +1,4 @@
+// Function to show the table
 function showTable() {
     var divTable = document.getElementById("div-table");
     var tr = document.createElement("tr");
@@ -15,7 +16,20 @@ function showTable() {
     tr.appendChild(td3);
 }
 
+// Function to show the radio button text
 function showRbText(element) {
     let text = element.value;
     document.getElementById("rb-output").innerHTML = text + " was selected.";
+}
+
+// Global variable of button in order to listen to its event
+var addButton = document.createElement("button");
+addButton.innerHTML = "Click the new button"
+addButton.addEventListener("click", function() {
+    alert("The new button was clicked.");
+})
+// Function to show the button
+function showButton() {
+    var divButtonShow = document.getElementById("div-button-show");
+    divButtonShow.appendChild(addButton);
 }
